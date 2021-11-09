91a6d44e2440bde73997e60b51a5b2f915bda378
Daniele Manuzzi <daniele.manuzzi@cern.ch>
2021-11-09 12:47:20 +0100

    commit after using. fit KK_0.1_2018 still failing

--- run/runAll.sh
+++ run/runAll.sh
@@ -7,10 +7,13 @@
 #${B2HH_RUN}/Data/runAll.sh
 #condor_wait ${B2HH_LOG}/Data/log/DataMerge.txt
 
-years="201516__2017s29r2p2__2018"
+#years="201516__2017s29r2p2__2018"
+#years="2017s29r2p2__2018"
+years="2017s29r2p2"
 magnets="Tot"
 cuts_pid="kpi_5.-2.-5.3__pik_-5.3.5.-2__pipi_-2.3.-2.3__kk_2.-2.2.-2"
-cuts_bdt="KK_0.04__PIPI_0.12__KK_0.1__PIPI_0.2"
+#cuts_bdt="KK_0.04__PIPI_0.12__KK_0.1__PIPI_0.2"
+cuts_bdt="KK_0.1"
 binnings="71_10_1_6"
 effNoFid="0_0_0_0"
 Ncpu="40"
@@ -59,9 +62,9 @@ Ncpu="40"
 #condor_wait ${B2HH_LOG}/Tagging/log/Tagging.txt
 
 ## FitTotal
-rm -rf  ${B2HH_LOG}/FitTotal/log/FitTotal.txt
-${B2HH_RUN}/FitTotal/runAll.sh ${years} ${magnets} ${cuts_bdt} ${Ncpu}
+#rm -rf  ${B2HH_LOG}/FitTotal/log/FitTotal.txt
+#${B2HH_RUN}/FitTotal/runAll.sh ${years} ${magnets} ${cuts_bdt} ${Ncpu}
 
 ## Plots FitTotal
-# condor_wait ${B2HH_LOG}/FitTotal/log/FitTotal.txt
-# ${B2HH_RUN}/FitTotal/runAllPlots.sh ${years} ${magnets} ${cuts_bdt} 1
+condor_wait ${B2HH_LOG}/FitTotal/log/FitTotal.txt
+${B2HH_RUN}/FitTotal/runAllPlots.sh ${years} ${magnets} ${cuts_bdt} 1
