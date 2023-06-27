71b2a745002f82ec10b70670a5ff175b515f6840
Daniele Manuzzi <Daniele Manuzzi daniele.manuzzi@cern.ch>
2023-06-27 14:45:20 +0200

    clean up

--- run/runAll.sh
+++ run/runAll.sh
@@ -20,15 +20,15 @@ magnets="Tot"
 #magnets="Tot__Up__Down"
 cuts_pid="kpi_5.-2.-5.3__pik_-5.3.5.-2__pipi_-2.3.-2.3__kk_2.-2.2.-2"
 #cuts_bdt="KK_0.1__PIPI_0.2"
-cuts_bdt="KK_0.1"
+cuts_bdt="KK_0.1_KK_0.2_KK_0.3_KK_0.4"
 #cuts_bdt="PIPI_0.2"
 binnings="71_10_1_6"
 effNoFid="0_0_0_0"
 Ncpu="56"
 
 ## REDUCE
-#rm -f ${B2HH_LOG}/Reduce/log/Reduce.txt
-#${B2HH_RUN}/Reduce/runAll.sh ${years} ${magnets} ${cuts_bdt}
+rm -f ${B2HH_LOG}/Reduce/log/Reduce.txt
+${B2HH_RUN}/Reduce/runAll.sh ${years} ${magnets} ${cuts_bdt}
 
 ## sPlot
 #rm -f ${B2HH_LOG}/sPlot/log/sPlot.txt
@@ -74,5 +74,5 @@ Ncpu="56"
 #${B2HH_RUN}/FitTotal/runAll.sh ${years} ${magnets} ${cuts_bdt} ${Ncpu}
 
 ## Plots FitTotal
-condor_wait ${B2HH_LOG}/FitTotal/log/FitTotal.txt
-${B2HH_RUN}/FitTotal/runAllPlots.sh ${years} ${magnets} ${cuts_bdt} 1
+#condor_wait ${B2HH_LOG}/FitTotal/log/FitTotal.txt
+#${B2HH_RUN}/FitTotal/runAllPlots.sh ${years} ${magnets} ${cuts_bdt} 1
